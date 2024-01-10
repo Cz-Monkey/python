ddf2fbda5628be1c9ff21946196f349e49ed6759
Hyunseok Jin <gustjrdlaek@gmail.com>
2024-01-10 11:03:36 +0900

    파이썬 todo 개념까지

---
+++ day8/006.py
@@ -0,0 +1,18 @@
+# for문
+# hello를 for를 이용해 3번 출력하시오
+# for x in (1,2,3):
+#     print('hello')
+
+# for x in range(3):
+#     print(x)
+
+# for를 이용해서 0~10사이의 짝수를 출력
+# for x in range(11):
+#     if x % 2 == 0:
+#         print(x)
+
+# 1부터 5의 합계를 출력 : 15
+result = 0
+for i in range(1,6):
+    result = result + i
+print(result)
