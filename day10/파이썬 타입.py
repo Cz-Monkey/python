f7c40d917602a079c6bcd4a2f5a449cc499a8eaf
Ekarusz <tjwnsdn1207@gmail.com>
2024-01-18 08:36:31 +0900

    2024-01-17 pytest까지

--- day10/파이썬 타입.py
+++ day10/파이썬 타입.py
@@ -1,2 +1,11 @@
 # 파이썬 타입 : int, float, str, bool, list, tuple, dictionary, set
 # 파이썬으로 타입 자체를 만들 수 있다 -> 사용자 정의 타입 : class
+# 타입은 필요한 데이터를 저장하고 연산을 수행할 수 있다
+# 내가 만든 type인 클래스의 변수를 object(객체)라고 한다
+
+str = 'hello'
+# 파이썬의 모든 타입은 클래스다
+# 파이썬의 모든 변수는 객체다
+# 객체에 소속된 함수를 메소드라고 한다
+print(type(str))
+print(len(str))
--- day13/func001.py
+++ day13/func001.py
@@ -1,18 +1,18 @@
-def sample1():
-    return 10
+# def sample1():
+#     return 10
 
-# 나이를 입력받아 성년/미성년을 판단
-def is_major(nai:int)->bool:
-    return nai >= 18
+# # 나이를 입력받아 성년/미성년을 판단
+# def is_major(nai:int)->bool:
+#     return nai >= 18
 
-# 나이를 입력받아 입장료를 리턴하는 함수
-# 25~64세면 3000원, 기타는 무료
+# # 나이를 입력받아 입장료를 리턴하는 함수
+# # 25~64세면 3000원, 기타는 무료
 
-def is_nai(num:int)->int:
-    if num > 24 and num <= 64:
-        return 3000
-    else:
-        return 0
+# def is_nai(num:int)->int:
+#     if num > 24 and num <= 64:
+#         return 3000
+#     else:
+#         return 0
     
 # 입장료는 3000원이다. 10명이면 1인당 2400원이다
 # 인원수를 입력받아 요금을 출력
